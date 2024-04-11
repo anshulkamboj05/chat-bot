2037643c52844d1c11cd22f149a7d938a000b2b6
Anshul Kamboj <125079825+anshulkamboj05@users.noreply.github.com>
2024-04-10 19:47:26 -0600

    start

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default  function home(){
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5x1 w-full items-center justify-between font-mono text-sm 1">
+      <div>
+
+      </div>
+
+    </div>
+    </main>
+  );
+}
+
+
